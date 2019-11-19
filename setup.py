fd27eb438ecfaaa0f23aadb057500c39af02d9d8
Sergey Chikuyonok <serge.che@gmail.com>
2019-11-19 14:08:14 +0300

    Include JSON files in dist package

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-emmet",
-    version="1.0.0",
+    version="1.0.1",
     author="Sergey Chikuyonok",
     author_email="serge.che@gmail.com",
     description="Emmet is a web-developer’s toolkit for boosting HTML & CSS code writing.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/emmetio/py-emmet",
+    include_package_data=True,
     packages=setuptools.find_packages(exclude=('tests', 'tests.*',)),
     classifiers=[
         "Programming Language :: Python :: 3",
